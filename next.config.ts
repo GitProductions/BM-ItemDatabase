52514c38b9cc6eff5c046fcea921694890892f10
Gitago <76603653+gitagogaming@users.noreply.github.com>
2026-01-27 17:36:45 -0500

    update nextconfig  for static build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: { unoptimized: true },
+  
   reactCompiler: true,
   turbopack: {
     // Force Turbopack to treat this folder as the project root to avoid mis-detected lockfiles higher up the tree.
