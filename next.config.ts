48a154dd2334ba17a20aa7dfc59ce7603245505b
Gitago <76603653+gitagogaming@users.noreply.github.com>
2026-01-27 17:38:09 -0500

    disable turbopack

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   images: { unoptimized: true },
-  
+
   reactCompiler: true,
-  turbopack: {
-    // Force Turbopack to treat this folder as the project root to avoid mis-detected lockfiles higher up the tree.
-    root: __dirname,
-  },
+  // turbopack: {
+  //   // Force Turbopack to treat this folder as the project root to avoid mis-detected lockfiles higher up the tree.
+  //   root: __dirname,
+  // },
 };
 
 export default nextConfig;
