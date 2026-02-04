14eda11ca0233699dfbcb0f68f361a984b48adcd
Gitago <76603653+gitagogaming@users.noreply.github.com>
2026-02-04 16:03:03 -0500

    .

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,13 +1,3 @@
-/* eslint-disable import/no-anonymous-default-export, @typescript-eslint/no-unused-vars */
-// import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-// import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-// import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
-
-// export default defineCloudflareConfig({
-//   incrementalCache: r2IncrementalCache,
-//   queue: doQueue,
-// });
-
 // open-next.config.ts
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 // import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
